35a5165541376e397ab7013903b9d9b920791085
DavidMwangi <david.mathenge98@gmail.com>
2017-04-11 15:50:57 +0300

    Main page view

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def main_page():
+
+    output = '''
+
+      <html>
+        <head><title>%s</title></head>
+          <body>
+             <h1>%s</h1><p>%s</p>
+          </body>
+      </html>
+
+      ''' %(
+
+          'Django Bookmarks',
+          'Welcome to Django Bookmarks',
+          'You can store and share bookmarks!'
+          )
+
+    return HttpResponse(output)
 
 # Create your views here.
